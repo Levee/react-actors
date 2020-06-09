cfccc05ea460309cf632e8c2d280033a69a40389
Levi <levicarlson123@gmail.com>
2020-06-09 12:33:28 -0500

    added inputs to contact.js

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -5,6 +5,10 @@ class Contact extends Component {
     return (
       <div>
         <h2>Contact Us</h2>
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+        <input type="text" placeholder="Best Impression" />
+        <button id="submit">Join Us!</button>
         <p>Phone: 1-800-REACTOR</p>
         <p>Email: info@ReactActors.com</p>
       </div>
